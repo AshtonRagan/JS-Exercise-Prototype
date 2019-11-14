ea082f8f6fdb3c4ae07aebc86dd306031b03949b
ArkRaga <53240640+ArkRaga@users.noreply.github.com>
2019-11-14 16:08:32 -0500

    updateding some changes

--- index.js
+++ index.js
@@ -98,7 +98,9 @@ function Baby(name, age, favoriteToy) {
   Person.call(this, name, age);
   this.favoriteToy = favoriteToy;
 }
+
 Baby.prototype = Object.create(Person.prototype);
+
 Baby.prototype.play = function() {
   return `Playing with ${this.favoriteToy}`;
 };
